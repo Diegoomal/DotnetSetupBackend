b8a701180774a948b668938d9b2e82bff82a5891
Diego Vasque Maldonado <diegoomal13@gmail.com>
2024-01-07 22:22:54 -0300

    update

--- SetupBackend.Console/Program.cs
+++ SetupBackend.Console/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Person p = new Person() { Name = "Diego" };
+Person p = new Person() { Name = "Name", Lastname = "Lastname" };
 System.Console.WriteLine(p.ToString());
